9b79c76eeebf4d93430574a5af223127b7561d5b
Muhammad Saad Bhinder <msjbhinder@gmail.com>
2024-06-25 12:47:37 +0500

    Rename file from 'User' to 'user' in models directory

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { hash } from "bcrypt";
 
 export const getAllUsers = async (
