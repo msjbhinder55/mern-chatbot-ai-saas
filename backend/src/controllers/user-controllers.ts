d9d4b0a0a6a9e910062ef7d230ea39ab196efdd1
Muhammad Saad Bhinder <msjbhinder@gmail.com>
2024-06-30 17:32:30 +0500

    feat: Set up data validation middleware with express-validator
    - Integrated express-validator library for request validation
    - Added validation middleware for user signup and other routes

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -27,7 +27,7 @@ export const userSignUp = async (
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
-    return res.status(200).json({ message: "Ok", id: user._id.toString() });
+    return res.status(201).json({ message: "Ok", id: user._id.toString() });
   } catch (error) {
     console.log(error);
     return res.status(200).json({ message: "Error", cause: error.message });
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAllUsers, userSignUp } from "../controllers/user-controllers.js";
+import { signupValidator, validate } from "../utils/validators.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
 
-userRoutes.post("/signup", userSignUp);
+userRoutes.post("/signup", validate(signupValidator), userSignUp);
 
 export default userRoutes;
