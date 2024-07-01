673a00cf26065b2861cfc7a649043254d5f3f70b
Muhammad Saad Bhinder <msjbhinder@gmail.com>
2024-07-01 07:32:39 +0500

    feat: Implement JWT tokens for authentication
    - Integrated JWT token generation and verification
    - Updated login route to generate and return JWT tokens
    - Added middleware to protect routes using JWT authentication

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/user.js";
 import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -53,6 +54,9 @@ export const userLogin = async (
     if (!isPasswordCorrect) {
       return res.status(403).send("Incorrect Password");
     }
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+
     return res.status(200).json({ message: "Ok", id: user._id.toString() });
   } catch (error) {
     console.log(error);
