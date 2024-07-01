9820fc346cccdf27c3f3bdadd154082648c0113b
Muhammad Saad Bhinder <msjbhinder@gmail.com>
2024-07-01 08:21:57 +0500

    feat: Set HTTP-only cookies with cookie-parser
    - Integrated cookie-parser middleware for handling cookies
    - Set up HTTP-only cookies for secure token storage
    - Updated login route to set JWT token in HTTP-only cookie

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import User from "../models/user.js";
 import { hash, compare } from "bcrypt";
 import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -30,6 +31,24 @@ export const userSignUp = async (
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "Ok", id: user._id.toString() });
   } catch (error) {
     console.log(error);
@@ -55,7 +74,23 @@ export const userLogin = async (
       return res.status(403).send("Incorrect Password");
     }
 
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+
     const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
 
     return res.status(200).json({ message: "Ok", id: user._id.toString() });
   } catch (error) {
