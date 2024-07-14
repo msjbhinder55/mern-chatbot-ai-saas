61124f6a90b8d04256ed1ca6fe2cabba2350f8a5
Muhammad Saad Bhinder <msjbhinder@gmail.com>
2024-07-14 17:24:25 +0500

    feat: Implement functionality to delete all user chats
    - Added API endpoint to handle deletion of all user chats
    - Integrated frontend with backend to send delete request
    - Updated state management to remove chats from UI after deletion
    - Included confirmation prompt before deleting all chats for safety

--- backend/src/controllers/chat-controllers.ts
+++ backend/src/controllers/chat-controllers.ts
@@ -60,3 +60,27 @@ export const sendChatsToUser = async (
     return res.status(200).json({ message: "ERROR", cause: error.message });
   }
 };
+
+export const deleteChats = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //user token check
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user) {
+      return res.status(401).send("User not registered OR Token malfunctioned");
+    }
+    if (user._id.toString() !== res.locals.jwtData.id) {
+      return res.status(401).send("Permissions didn't match");
+    }
+    //@ts-ignore
+    user.chats = [];
+    await user.save();
+    return res.status(200).json({ message: "OK" });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyToken } from "../utils/token-manager.js";
 import { chatCompletionValidator, validate } from "../utils/validators.js";
 import {
+  deleteChats,
   generateChatCompletion,
   sendChatsToUser,
 } from "../controllers/chat-controllers.js";
@@ -15,5 +16,6 @@ chatRoutes.post(
   generateChatCompletion
 );
 chatRoutes.get("/all-chats", verifyToken, sendChatsToUser);
+chatRoutes.delete("/delete", verifyToken, deleteChats);
 
 export default chatRoutes;
