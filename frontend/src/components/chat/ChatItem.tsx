bd252735e40f300c7bdf388b8ff43260043e9a91
Muhammad Saad Bhinder <msjbhinder@gmail.com>
2024-07-14 15:50:57 +0500

    feat: Design chat page UI on frontend
    - Created a responsive and user-friendly chat page interface
    - Implemented chat input field and message display area
    - Styled chat components to enhance visual appeal and user experience
    - Added functionality to send and receive messages

---
+++ frontend/src/components/chat/ChatItem.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Box, Avatar, Typography } from "@mui/material";
+import { useAuth } from "../../context/AuthContext";
+
+function extractCodeFromString(message: string) {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+}
+
+function isCodeBlock(str: string) {
+  if (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("[") ||
+    str.includes("]") ||
+    str.includes("{") ||
+    str.includes("}") ||
+    str.includes("#") ||
+    str.includes("//")
+  ) {
+    return true;
+  }
+  return false;
+}
+const ChatItem = ({
+  content,
+  role,
+}: {
+  content: string;
+  role: "user" | "assistant";
+}) => {
+  const messageBlocks = extractCodeFromString(content);
+  const auth = useAuth();
+  return role == "assistant" ? (
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d5612",
+        gap: 2,
+        borderRadius: 2,
+        my: 1,
+      }}
+    >
+      <Avatar sx={{ ml: "0" }}>
+        <img src="openai.png" alt="openai" width={"30px"} />
+      </Avatar>
+    </Box>
+  ) : (
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d56",
+        gap: 2,
+        borderRadius: 2,
+      }}
+    >
+      <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
+        {auth?.user?.name[0]}
+        {auth?.user?.name.split(" ")[1][0]}
+      </Avatar>
+    </Box>
+  );
+};
+
+export default ChatItem;
